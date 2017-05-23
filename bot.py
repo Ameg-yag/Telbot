33bf28670963e73fd04e62baf3005e373e1963bf
Soporte <soporte@micentinela.com>
2017-05-23 16:51:15 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -170,7 +170,7 @@ def info_opt(m):
             print(color.BLUE + "[+] PING..." + color.ENDC)
             ping = os.system("ping -c 4 www.micentinela.com")
             bot.send_message(cid, open(ping))
-            print(color.GREEN + " [ii] Usado: %s" % ping + color.ENDC)
+            #print(color.GREEN + " [ii] Usado: %s" % ping + color.ENDC)
 
             
             
