36407c40df3f1b0e3895e968558837d8eb6f49fd
Soporte <soporte@micentinela.com>
2017-05-27 19:06:16 +0000

    Update client.py

--- client.py
+++ client.py
@@ -7,10 +7,11 @@ import telebot
 TOKEN = "347644672:AAHu5fbP7ot0p98XJLwEUiv-ZH_Trwm9Ybo" # Cambiar por el token
 bot = telebot.TeleBot(TOKEN)
 cid = "7506285" # Cambiar por el numero cid
-ip = "192.168.1.103"
+#ip = "192.168.1.103"
 puerto = 80
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((ip, puerto))
+#s.bind((ip, puerto))
+s.bind(('', puerto))
 s.listen(1)
 while True:
         conn, addr = s.accept()
